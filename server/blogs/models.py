a4feb9b184663a364acf35f480b88b3fb5770ca0
J0YERIM <chosarah70@gmail.com>
2023-02-20 02:42:05 +0900

    ãmodeling

--- server/blogs/models.py
+++ server/blogs/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='blog')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.CharField(max_length=128)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='posts')
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=128)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_categories')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='post_categories')
+
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_tags')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='post_tags')
--- server/conf/settings.py
+++ server/conf/settings.py
@@ -52,7 +52,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'blogs',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -131,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
