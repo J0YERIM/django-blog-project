e889a3d2968c6f0c0a8b6939ef7d16eb0b88c1e0
J0YERIM <chosarah70@gmail.com>
2023-02-20 12:14:21 +0900

    add __str__

--- server/blogs/models.py
+++ server/blogs/models.py
@@ -7,6 +7,9 @@ class Blog(models.Model):
     description = models.CharField(max_length=128)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='blog')
 
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -14,6 +17,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -23,25 +29,43 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
+    def __str__(self):
+        return self.content
+
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
 
+    def __str__(self):
+        return self.user
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
 
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_categories')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='post_categories')
 
+    def __str__(self):
+        return f"{self.post} : {self.category}"
+
 
 class PostTag(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_tags')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='post_tags')
+
+    def __str__(self):
+        return f"{self.post} : {self.tag}"
--- server/users/models.py
+++ server/users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user/', default='default.png')
+
+    def __str__(self):
+        return self.username
